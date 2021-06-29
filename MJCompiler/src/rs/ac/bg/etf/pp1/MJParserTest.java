65d65c0ec993bf909779ae5dac2ebe3f9ca70bae
nkrstic98 <nikolakrle998@gmail.com>
2021-06-29 22:12:57 +0200

    Dodat ispis u output fajl, i fajl sa greskama

--- MJCompiler/src/rs/ac/bg/etf/pp1/MJParserTest.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/MJParserTest.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
@@ -95,16 +96,98 @@ public class MJParserTest implements Compiler {
 		return compilerErrors.size() > 0 ? compilerErrors : null;
 	}
 	
+
+	public void writeOutputFile() {
+		FileReader fr = null;
+		FileWriter fw = null;
+		
+		try {
+			fr = new FileReader("logs/mj-test.log");
+			fw = new FileWriter("test/izlaz.out");
+			
+			String toCopy = "";
+			
+			int i;
+			
+			while((i = fr.read()) != -1) {
+				toCopy += (char)i;
+			}
+			
+			fw.write(toCopy);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(fr != null) fr.close();
+				if(fw != null) fw.close();
+			}
+			catch (IOException e) {
+				System.err.println("Greska prilikom zatvaranja fajlova prilikom upisa standardnog izlaza");
+			}
+		}
+	}
+	
+	public void writeErrorsToFile(List<CompilerError> errorList) {
+		FileOutputStream fos = null;
+		File file;
+		
+		try {
+			file = new File("test/izlaz.err");
+			
+			fos = new FileOutputStream(file);
+			
+			if(file.exists()) file.createNewFile();
+			
+			byte[] bytesArray;
+			for (CompilerError err : errorList) {
+				bytesArray = (err.toString() + "\n").getBytes();
+				fos.write(bytesArray);
+				fos.flush();
+			}
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(fos != null) {
+					fos.close();
+				}
+			}
+			catch (IOException e) {
+				System.err.println("Greska prilikom zatvaranja error fajla");
+			}
+		}
+	}
+	
+	
 	public static void main(String[] args) throws Exception {
 		Logger log = Logger.getLogger(MJParserTest.class);
 		
 		MJParserTest compiler = new MJParserTest();
+		
 		List<CompilerError> errors = compiler.compile(args[0], args[1]);
 		
 		if(errors != null) {
-			for (CompilerError err : errors) {
-				log.error(err.toString());
+//			for (CompilerError err : errors) {
+//				log.error(err.toString());
+//			}
+			
+			if(args.length == 3 && args[2].equalsIgnoreCase("2>izlaz.err")) {
+				compiler.writeErrorsToFile(errors);
 			}
+			else if(args.length == 4 && (args[2].equalsIgnoreCase("2>izlaz.err") || args[3].equalsIgnoreCase("2>izlaz.err"))) {
+				compiler.writeErrorsToFile(errors);
+			}
+		}
+		
+		if(args.length == 3 && args[2].equalsIgnoreCase(">izlaz.out")) {
+			compiler.writeOutputFile();
+		}
+		else if(args.length == 4 && (args[2].equalsIgnoreCase(">izlaz.out") || args[3].equalsIgnoreCase(">izlaz.out"))) {
+			compiler.writeOutputFile();
 		}
 	}
 }
